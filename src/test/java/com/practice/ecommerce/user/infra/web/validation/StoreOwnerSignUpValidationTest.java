ca3e6f95588ebbe2468559245e7b5315c1543a20
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-22 15:17:10 +0900

    🔬TEST: API Validation 테스트 개발

---
+++ src/test/java/com/practice/ecommerce/user/infra/web/validation/StoreOwnerSignUpValidationTest.java
@@ -0,0 +1,92 @@
+package com.practice.ecommerce.user.infra.web.validation;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.ecommerce.docsUtils.Identifier;
+import com.practice.ecommerce.docsUtils.VirtualStoreOwner;
+import com.practice.ecommerce.user.application.service.UserService;
+import com.practice.ecommerce.user.docs.StoreOwnerSignUpDocs;
+import com.practice.ecommerce.user.infra.web.dto.StoreOwnerRegisterRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+@WebMvcTest
+@AutoConfigureRestDocs
+@DisplayName("스토어 오너 - 회원가입API - 유효성 테스트")
+public class StoreOwnerSignUpValidationTest {
+
+	@Autowired
+	MockMvc mockMvc;
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	@MockBean
+	private UserService userService;
+
+
+	@AfterEach
+	void tearDown() {
+		VirtualStoreOwner.clear();
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_password() throws Exception {
+		VirtualStoreOwner.setPassword(null);
+		StoreOwnerRegisterRequest request = StoreOwnerSignUpDocs.Request.create();
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_loginId() throws Exception {
+		VirtualStoreOwner.setLoginId(null);
+		StoreOwnerRegisterRequest request = StoreOwnerSignUpDocs.Request.create();
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_address() throws Exception {
+		VirtualStoreOwner.setAddress(null);
+		StoreOwnerRegisterRequest request = StoreOwnerSignUpDocs.Request.create();
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_nickName() throws Exception {
+		VirtualStoreOwner.setNickName(null);
+		StoreOwnerRegisterRequest request = StoreOwnerSignUpDocs.Request.create();
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+	private ResultActions callController(StoreOwnerRegisterRequest request) throws Exception {
+
+		StoreOwnerSignUpDocs docs = new StoreOwnerSignUpDocs(null);
+
+		return mockMvc.perform(RestDocumentationRequestBuilders.post("/store-owner/sign-up")
+				.contentType(MediaType.APPLICATION_JSON_VALUE)  // Set Content-Type explicitly
+				.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andDo(docs.validationFilterInMockMvc(Identifier.STORE_OWNER_SIGN_UP_400_BAD));
+	}
+}
---
+++ src/test/java/com/practice/ecommerce/user/infra/web/validation/StoreOwnerUpdatePasswordValidationTest.java
@@ -0,0 +1,81 @@
+package com.practice.ecommerce.user.infra.web.validation;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.ecommerce.docsUtils.Identifier;
+import com.practice.ecommerce.docsUtils.VirtualStoreOwner;
+import com.practice.ecommerce.user.application.service.UserService;
+import com.practice.ecommerce.user.docs.StoreOwnerUpdatePasswordDocs;
+import com.practice.ecommerce.user.infra.web.StoreOwnerController;
+import com.practice.ecommerce.user.infra.web.dto.UserPasswordUpdateRequest;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
+@WebMvcTest
+@AutoConfigureRestDocs
+@DisplayName("스토어 오너 - 비밀번호 변경 - 유효성 테스트")
+public class StoreOwnerUpdatePasswordValidationTest {
+
+	@Autowired
+	MockMvc mockMvc;
+	ObjectMapper objectMapper = new ObjectMapper();
+
+	@MockBean
+	private UserService userService;
+
+	private StoreOwnerUpdatePasswordDocs docs = new StoreOwnerUpdatePasswordDocs(null);
+
+	@AfterEach
+	void tearDown() {
+		VirtualStoreOwner.clear();
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_newPassword() throws Exception {
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute("id", VirtualStoreOwner.getLoginId());
+		UserPasswordUpdateRequest request = StoreOwnerUpdatePasswordDocs.requestCreate(null);
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+	@Test
+	public void 회원가입_유효성_검사_Password() throws Exception {
+		VirtualStoreOwner.setPassword(null);
+		UserPasswordUpdateRequest request = StoreOwnerUpdatePasswordDocs.requestCreate("newPassword");
+
+		callController(request)
+			.andExpect(jsonPath("message").value("잘못된 Body 입니다. 문서를 참고하여 올바른 데이터를 첨부해주세요."));
+	}
+
+
+
+	private ResultActions callController(UserPasswordUpdateRequest request) throws Exception {
+
+		return mockMvc.perform(
+				RestDocumentationRequestBuilders.patch("/store-owner/password")
+					.contentType(MediaType.APPLICATION_JSON_VALUE)
+					.content(objectMapper.writeValueAsString(request))
+			)
+			.andDo(print())
+			.andExpect(status().isBadRequest())
+			.andDo(docs.validationFilterInMockMvc(Identifier.STORE_OWNER_UPDATE_PASSWORD_400_BAD));
+
+	}
+}
